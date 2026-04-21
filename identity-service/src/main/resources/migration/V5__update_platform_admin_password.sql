740b64dc91eb1b90c3807c4423d0234405574c49
DeveloperHarish98 <harishdubey98r@gmail.com>
2026-04-21 11:01:10 +0530

    added env configuration

--- identity-service/src/main/resources/migration/V5__update_platform_admin_password.sql
+++ identity-service/src/main/resources/migration/V5__update_platform_admin_password.sql
@@ -4,6 +4,6 @@
 -- BCrypt cost-12 hash for 'ChangeMe123!'.
 -- ============================================================
 
-UPDATE admin_users 
+UPDATE admin_users
 SET password_hash = '$2a$12$AoYNbd97YTLItW5oBV7neeLYwAerO.SAumw1D2qhyjFz8oDRz73iG'
 WHERE email = 'mat@loyalty360.io';
